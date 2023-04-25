824cc23f9c0eb8ccca24de89a02dd86da515900d
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2023-04-25 13:34:13 +0100

    Added additional comparisons with individual acoustic indices, weather and health, but no obvious consistent patterns.

--- basic_visualisation.R
+++ basic_visualisation.R
@@ -115,6 +115,10 @@ plot(acoustic_pca, display = "species")
 plot(acoustic_pca, display = "sites")
 acoustic_sco <- data.frame(scores(acoustic_pca, display="sites"))
 
+ggplot(tmp, aes(x=acoustic_sco$PC1, y=acoustic_sco$PC2, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+
 ggplot(tmp, aes(x=date, y=acoustic_sco$PC1, colour=colony)) +
   geom_point() +
   facet_wrap(~site)
@@ -143,12 +147,118 @@ ggplot(tmp, aes(x = cbpv_status1, y = acoustic_sco$PC1, colour=colony)) +
 ggplot(tmp, aes(x = cbpv_status1, y = acoustic_sco$PC2, colour=colony)) +
   geom_boxplot() +
   facet_wrap(~site)
+# Some individual acoustics with disease
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = ACI, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = RMS, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = mean_dfreq, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = H, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = M, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = Ht, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq_mean, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = sd, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq_median, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = sem, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq_mode, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = Q25, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = Q75, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = IQR, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = cent, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = skewness, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = kurtosis, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = sfm, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = sh, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = prec, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = time.P1, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = time.M, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = time.P2, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = time.IPR, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq.P1, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq.M, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq.P2, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = varroa_per_300_bees1, y = freq.IPR, colour=colony)) +
+  geom_point() +
+  facet_wrap(~site)
+# Less easy to interpret boxplots
+ggplot(tmp, aes(x = cbpv_status1, y = freq_mode, colour=colony)) +
+  geom_boxplot() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = cbpv_status1, y = RMS, colour=colony)) +
+  geom_boxplot() +
+  facet_wrap(~site)
 
 # A few correlations of acoustic stuff
 tmp3 <- cbind(tmp2, acoustic_sco)
 library(ggcorrplot)
 corr <- cor(tmp3)
 ggcorrplot(corr, hc.order = TRUE)
+
+# Weather over time
+ggplot(tmp, aes(x = date, y = temperature)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = date, y = windsp)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = date, y = rain_mm_h_mean)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site)
 
 
 # Some comparisons of acoustics with weather
@@ -159,5 +269,11 @@ ggplot(tmp, aes(x = windsp, y = ACI, colour = colony)) +
   geom_point() +
   facet_wrap(~site)
 ggplot(tmp4, aes(x = rain_mm_h_mean, y = mean_dfreq, colour = colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = temperature, y = freq_mode, colour = colony)) +
+  geom_point() +
+  facet_wrap(~site)
+ggplot(tmp, aes(x = windsp, y = freq_mode, colour = colony)) +
   geom_point() +
   facet_wrap(~site)
