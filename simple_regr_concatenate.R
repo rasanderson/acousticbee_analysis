edc83b32c39be2428a7ac2024c7f056b4356164e
Roy Sanderson <roy.sanderson@ncl.ac.uk>
2023-05-11 10:33:44 +0100

    Cleaned up code so easier for Liam to interpret

--- simple_regr_concatenate.R
+++ simple_regr_concatenate.R
@@ -8,9 +8,11 @@ library(ggplot2)
 library(dplyr)
 library(lubridate)
 library(tidyr)
+library(keras)
+library(rsample)
 
 
-# Read in and pre-process data ####
+# Read in, clean and pre-process data ####
 rawd <- read.csv("data/All_Data_Master_Shortened.csv")
 # Three rows with missing values causing trouble need removing
 rawd <- rawd[!is.na(rawd$ACI),]
@@ -29,10 +31,7 @@ data_subset$cos_hour <- cos((2 * pi * data_subset$hour ) / 24)
 data_subset$sin_hour <- sin((2 * pi * data_subset$hour ) / 24)
 data_subset$grid_easting2 <- data_subset$grid_easting^2
 data_subset$grid_northing2 <- data_subset$grid_northing^2
-
-# Begin with just the "meta-data" about the environment ####
-# Work on the Varroa as more variable response
-# However, no records at Thorganby as not assessed. Remove missing values.
+# No records at Thorganby site as not assessed. Remove missing values.
 # Note there are 175 NA for station_easting/northing, but 320 for grid_easting/n
 data_subset <- filter(data_subset, site != "Thorganby") %>% 
   drop_na(grid_easting) %>% 
@@ -41,43 +40,32 @@ data_subset <- filter(data_subset, site != "Thorganby") %>%
   drop_na(windsp) %>% 
   drop_na(temperature) 
 
-# Split data first, so meta- and audio- using same data points
-library(keras)
-library(rsample)
-# Add random seed. Tricky to evaluate which changes are random vs which due
-# to model structure in some setups
-set_random_seed(123)
 
+
+# Split data into train and test, meta- and audio- using same time points ####
+# Add random seed for consistency between runs
+set_random_seed(123)
 split <- initial_split(data_subset, 0.8)
 split_train_dataset <- training(split)
 split_test_dataset <- testing(split)
 
+
+
+# Setup data for "meta-" variables ####
 meta_variables <- c("varroa_per_300_bees1", "day_of_year", "cos_hour", "sin_hour",
                     "grid_easting", "grid_northing", "rain_mm_h_mean", "windsp",
                     "temperature", "grid_easting2", "grid_northing2")
 meta_train_dataset <- select(split_train_dataset, all_of(meta_variables))
 meta_test_dataset <- select(split_test_dataset, all_of(meta_variables))
-
 # Now split features from labels
 meta_train_features <- meta_train_dataset %>% select(-varroa_per_300_bees1)
-meta_test_features <- meta_test_dataset %>% select(-varroa_per_300_bees1)
-
-meta_train_labels <- meta_train_dataset %>% select(varroa_per_300_bees1)
-meta_test_labels <- meta_test_dataset %>% select(varroa_per_300_bees1)
-
-# Normalise the data
-meta_normalizer <- layer_normalization(axis = -1L)
-# The adapt() function fits the state of the normalized data and fixes it so
-# it doesn't change subsequently
-meta_normalizer %>% adapt(as.matrix(meta_train_features))
-print(meta_normalizer$mean)
-first <- as.matrix(meta_train_features[1,])
-cat('First example:', first)
-cat('Normalized:', as.matrix(meta_normalizer(first)))
+meta_test_features  <- meta_test_dataset %>% select(-varroa_per_300_bees1)
+meta_train_labels   <- meta_train_dataset %>% select(varroa_per_300_bees1)
+meta_test_labels    <- meta_test_dataset %>% select(varroa_per_300_bees1)
 
 
 
-# Deep learning model for acoustic indices ####
+# Setup data for "acoustic" variables ####
 acoustic_variables <- c("varroa_per_300_bees1", "ACI", "RMS", "H", "mean_dfreq",
                         "M", "Ht", "freq_mean", "sd", "freq_median", "sem",
                         "freq_mode", "Q25", "Q75", "IQR", "cent", "skewness",
@@ -86,49 +74,35 @@ acoustic_variables <- c("varroa_per_300_bees1", "ACI", "RMS", "H", "mean_dfreq",
                         "freq.IPR")
 acoustic_train_dataset <- select(split_train_dataset, all_of(acoustic_variables))
 acoustic_test_dataset <- select(split_test_dataset, all_of(acoustic_variables))
-# # Smoothed freq histograms and scatterplots (slow)
-# train_dataset %>%
-#   select(day_of_year, cos_hour, sin_hour, temperature) %>%
-#   GGally::ggpairs()
-# # Range etc.
-# skimr::skim(meta_subset)
-
 # Now split features from labels
 acoustic_train_features <- acoustic_train_dataset %>% select(-varroa_per_300_bees1)
-acoustic_test_features <- acoustic_test_dataset %>% select(-varroa_per_300_bees1)
+acoustic_test_features  <- acoustic_test_dataset %>% select(-varroa_per_300_bees1)
+acoustic_train_labels   <- acoustic_train_dataset %>% select(varroa_per_300_bees1)
+acoustic_test_labels    <- acoustic_test_dataset %>% select(varroa_per_300_bees1)
 
-acoustic_train_labels <- acoustic_train_dataset %>% select(varroa_per_300_bees1)
-acoustic_test_labels <- acoustic_test_dataset %>% select(varroa_per_300_bees1)
-
-# Normalise the data
-acoustic_normalizer <- layer_normalization(axis = -1L)
-# The adapt() function fits the state of the normalized data and fixes it so
-# it doesn't change subsequently
-acoustic_normalizer %>% adapt(as.matrix(acoustic_train_features))
-print(acoustic_normalizer$mean)
-first <- as.matrix(acoustic_train_features[1,])
-cat('First example:', first)
-cat('Normalized:', as.matrix(acoustic_normalizer(first)))
-
-# For simplicity, just scale the features
+# Scale the meta and acoustic features as all in different units originally
 acoustic_train_features_scale <- scale(acoustic_train_features)
 meta_train_features_scale     <- scale(meta_train_features)
 
-# As relatively simple, use a single function to define and compile model
+
+
+# Create the DL model ####
+# Define upper branches of deep learning model for meta- and acoustic ####
+# meta-data part of DL model
 meta_dnn_model <- layer_input(shape = 10, name = "meta_dnn") %>% 
   layer_dense(64, activation = 'relu', name = "meta1") %>% 
   layer_dropout(0.25) %>% 
   layer_dense(10, activation = 'relu', name = "meta1") 
 
-# As relatively simple, use a single function to define and compile model
+# acoustic-indices part of DL model
 acoustic_dnn_model <- layer_input(shape = 28, name = "acoustic_dnn") %>%
     layer_dense(64, activation = 'relu', name = "acoustic1") %>% 
     layer_dropout(0.25) %>% 
     layer_dense(28, activation = 'relu', name = "acoustic1") 
 
+# Define the two top-most branches as inputs and concatenate together ####
 merge_inputs <-
   layer_concatenate(list(meta_dnn_model, acoustic_dnn_model), name = "conc") %>% 
-  #layer_dropout(0.25) %>% 
   layer_dense(38, activation = 'relu', name = "conc_dense")
 
 merge_outputs <- merge_inputs %>% 
@@ -147,6 +121,9 @@ combined_dnn %>% compile(
 summary(combined_dnn)
 plot(combined_dnn, show_shapes = TRUE, show_layer_names = TRUE, expand_nested = TRUE)
 
+
+
+# Finally fit the DL model ####
 # Outputs for target are same for both meta- and acoustic- datasets
 output_targets <- acoustic_train_labels
 
@@ -172,7 +149,7 @@ combined_test_results[['dnn_model']] <- combined_dnn %>% evaluate(
   verbose = 1
 )
 
-
+# Finish with graphs of observed vs predictions and r-squared
 sapply(combined_test_results, function(x) x)
 # Make some predictions
 combined_test_predictions <- predict(combined_dnn, list(as.matrix(scale(meta_test_features)), as.matrix(scale(acoustic_test_features))))
@@ -181,7 +158,9 @@ ggplot(test_obs_preds) +
   geom_point(aes(x = pred, y = varroa)) +
   geom_abline(intercept = 0, slope = 1, color = "blue") +
   geom_smooth(aes(x = pred, y = varroa), method = "lm", color = "red", se = FALSE)
-# Error distribution
+# Error distribution (not really needed)
 ggplot(test_obs_preds, aes(x = pred - varroa)) +
   geom_density()
+
+# Check r-squared of predictions for the test dataset
 cor(test_obs_preds)^2
