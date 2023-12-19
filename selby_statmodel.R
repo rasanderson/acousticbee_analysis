c0a7671daa0791ddba4d4fd053a85f3c7d0acba8
Roy Sanderson <roy.sanderson@newcastle.ac.uk>
2023-12-19 17:11:25 +0000

    Added Hexham varroa predictions, but under-predicts.

--- selby_statmodel.R
+++ selby_statmodel.R
@@ -72,12 +72,17 @@ selby_glm4 <- glmmTMB(log(varroa_per_300_bees1+1) ~ sin_day + cos_day +
                       data = selby_dat)
 summary(selby_glm4)
 simulateResiduals(selby_glm4, plot=TRUE)
+selby_glm4a <- glm(log(varroa_per_300_bees1+1) ~ sin_day + cos_day + 
+                        PC1 + PC2 + rain_mm_h_mean + temperature + windsp,
+                      data = selby_dat)
+summary(selby_glm4a)
+
 library(nlme)
-selby_glm4 <- gls(log(varroa_per_300_bees1+1) ~ sin_day + cos_day + 
+selby_glm4b <- gls(log(varroa_per_300_bees1+1) ~ sin_day + cos_day + 
                         PC1 + PC2 + rain_mm_h_mean + temperature + windsp,
                       data = selby_dat[!is.na(selby_dat$rain_mm_h_mean),])
-summary(selby_glm4)
-plot(ACF(selby_glm4))
+summary(selby_glm4b)
+plot(ACF(selby_glm4b))
 
 # Mixed effect
 selby_glm5 <- glmmTMB(log(varroa_per_300_bees1+1) ~ sin_day + cos_day + 
@@ -86,3 +91,29 @@ selby_glm5 <- glmmTMB(log(varroa_per_300_bees1+1) ~ sin_day + cos_day +
                       data = selby_dat)
 summary(selby_glm5)
 simulateResiduals(selby_glm5, plot=TRUE)
+
+
+# Try testing Selby-derived model glm4a vs Hexham
+hexham_dat <- filter(rawd, (date >= "2022-05-25" & date <= "2022-06-21") & site == "Hexham")
+hexham_dat <- filter(hexham_dat, !is.na(rain_mm_h_mean))
+hexham_dat <- filter(hexham_dat, !is.na(temperature))
+
+library(vegan)
+tmp2 <- hexham_dat[, 14:41]
+tmp2 <- decostand(tmp2, method = "hellinger")
+hexham_sco <- data.frame(predict(acoustic_pca, newdata = tmp2, type = "wa"))
+sin_hr <- sin(2*pi*hexham_dat$hour/24)
+cos_hr <- cos(2*pi*hexham_dat$hour/24)
+sin_day <- sin(2*pi*hexham_dat$day/365)
+cos_day <- cos(2*pi*hexham_dat$day/365)
+hexham_dat <- data.frame(cbind(hexham_dat), sin_hr, cos_hr,
+                        sin_day, cos_day, 
+                        PC1 = hexham_sco$PC1, PC2 = hexham_sco$PC2)
+
+hexham_pred <- predict(selby_glm4a, newdata = hexham_dat, type = "response")
+
+hexham_dat <- cbind(hexham_dat, hexham_pred)
+ggplot(hexham_dat, aes(x = date, y = varroa_per_300_bees1, colour = colony)) +
+  geom_smooth() 
+ggplot(hexham_dat, aes(x = date, y = exp(hexham_pred)-1, colour = colony)) +
+  geom_smooth() 
